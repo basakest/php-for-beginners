6e63ba0f5c3df4731ac3e8d8e4fdd563391ec05d
Songyongqiang <syq@Songyongqiangs-MacBook-Pro.local>
2020-05-22 21:25:14 +0800

    variables

---
+++ hello.php
@@ -0,0 +1,27 @@
+<?php
+    //echo 'hello world';
+    /*
+    $message = 'hello again';
+    $count = 5;
+    $price = 2.98;
+    //echo $message;
+    var_dump($message);
+    var_dump($count);
+    var_dump($price);
+    
+    $is_admin = false;
+    $value = null;
+    var_dump($is_admin, $value);
+    
+    $num1 = 5;
+    $num2 = 3.46;
+    $word1 = 'hello';
+    $word2 = 'nike';
+    $bollean1 = true;
+    $bollean2 = false;
+    var_dump($num1 * $num2, $word1 . ' ' . $word2, $bollean1 && $bollean2);
+    */
+    $word1 = 'hello';
+    $word2 = "{$word1} john, {$word1} alice";
+    echo $word2;
+
